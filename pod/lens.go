a91aa343e081129cb3b7759129dec3a3e1561020
Stan Rozenraukh <stan@stanistan.com>
2025-04-15 22:36:16 -0400

    DRY it up

--- pod/lens.go
+++ pod/lens.go
@@ -6,12 +6,23 @@ import (
 	"github.com/stanistan/mutator/internal/lens"
 )
 
-var (
-	initContainersLens = lens.Lens[[]corev1.Container, *corev1.Pod, Pod, MutatorFunc]{
-		Get:     func(pod *corev1.Pod) []corev1.Container { return pod.Spec.InitContainers },
-		Set:     func(pod *corev1.Pod, cs []corev1.Container) { pod.Spec.InitContainers = cs },
+func podLens[T any](
+	get func(*corev1.Pod) T,
+	set func(*corev1.Pod, T),
+) lens.Lens[T, *corev1.Pod, Pod, MutatorFunc] {
+	return lens.Lens[T, *corev1.Pod, Pod, MutatorFunc]{
+		Get:     get,
+		Set:     set,
 		ToInner: func(p Pod) *corev1.Pod { return p.Pod },
 	}
+}
+
+var (
+	initContainersLens = podLens(
+		func(pod *corev1.Pod) []corev1.Container { return pod.Spec.InitContainers },
+		func(pod *corev1.Pod, cs []corev1.Container) { pod.Spec.InitContainers = cs },
+	)
+
 	initContainersListLens = lens.ListLens[corev1.Container, *corev1.Pod, Pod, MutatorFunc]{
 		Lens:    initContainersLens,
 		Prepend: true,
